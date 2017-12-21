36d054d56f4b48a487b03ffec7e4581020be20ff
Brandon Levy <brandon.levy@mail.mcgill.ca>
2017-12-21 13:42:08 -0500

    completed exercise

--- js/main.js
+++ js/main.js
@@ -1,2 +1,17 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var body = document.querySelector('body')
+  var button = document.querySelector('.switch');
+  var text = document.querySelector('h1')
 
-
+  button.addEventListener('click', function(e){
+    if (e.target.classList.contains('on')){
+      e.target.className = 'switch off';
+      body.className = 'dark';
+      text.innerText = "Hey, who turned off the lights?"
+    }else if (e.target.classList.contains('off')){
+      e.target.className = 'switch on';
+      body.className = 'light';
+      text.innerText = "It's so bright in here!'"
+    }
+  })
+});
